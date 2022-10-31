d96343100ac8e8a2d5174a1de36a0cf2e6721c04
Ekjot Singh <workforekjot@gmail.com>
2022-10-31 16:13:04 +0530

    Excercise to print name 10 times

--- first.cpp
+++ first.cpp
@@ -4,7 +4,10 @@ int main()
 {
     std::cout<< "Hello World"<< std::endl;
     std::cout<< "I'm Ekjot Singh"<< std::endl;
-    
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout<<"Ekjot Singh"<< std::endl;
+    }
     
     return 0;
 }
