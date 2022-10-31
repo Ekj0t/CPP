969695c676e2a126a3304eb3f421461a79d7323e
Ekjot Singh <workforekjot@gmail.com>
2022-10-31 16:56:58 +0530

    errors and warnings in cpp

--- first.cpp
+++ first.cpp
@@ -3,18 +3,18 @@
 
 int main()
 {
-    std::cout<< "Hello World"<< std::endl;
-    std::cout<< "I'm Ekjot Singh"<< std::endl;
+    std::cout << "Hello World" << std::endl;
+    std::cout << "I'm Ekjot Singh" << std::endl;
     for (int i = 0; i < 10; i++)
     {
-        std::cout<<"Ekjot Singh"<< std::endl;
+        std::cout << "Ekjot Singh" << std::endl;
     }
     // Single Line Comment
     /*
-    This is a 
-    Multi Line 
+    This is a
+    Multi Line
     Comment
     */
-    
+
     return 0; // Program ends here
 }
