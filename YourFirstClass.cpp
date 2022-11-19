3231b8f3e4721f17ebb35670425ab01a40fcb75d
Ekjot Singh <workforekjot@gmail.com>
2022-11-19 20:37:50 +0530

    Trying Classes in cpp

---
+++ YourFirstClass.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+const long double PI{3.1415926535897932384626433832795};
+
+class Cylinder
+{
+public:
+    // Functions (methods)
+    double volume()
+    {
+        return PI * base_radius * base_radius * height;
+    }
+
+public:
+    // Member Variables(data)
+    double base_radius{1};
+    double height{1};
+};
+
+int main()
+{
+    Cylinder cylinder1; // Objects
+    std::cout << "volume: " << cylinder1.volume() << std::endl;
+    // Changing the member variables
+    cylinder1.base_radius = 10;
+    cylinder1.height = 3;
+    std::cout << "volume: " << cylinder1.volume() << std::endl;
+
+    cylinder1.height = 8;
+    std::cout << "volume: " << cylinder1.volume() << std::endl;
+    return 0;
+}
